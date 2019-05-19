bc9ac850542522143fd7f8768cfe90c774c20517
Uppercod <mtrujillo@outlook.cl>
2019-05-18 21:06:59 -0400

    add atomico-todo

---
+++ atomico-todo/rollup.config.js
@@ -0,0 +1,51 @@
+import pkg from "./package.json";
+import resolve from "rollup-plugin-node-resolve";
+import { terser } from "rollup-plugin-terser";
+import sucrase from "rollup-plugin-sucrase";
+import del from "rollup-plugin-delete";
+import importCss from "@atomico/rollup-plugin-import-css";
+import browsersync from "rollup-plugin-browsersync";
+import workbox from "@atomico/rollup-plugin-workbox";
+
+let publicDir = pkg.output.replace(/\/[^\/]+$/, "");
+
+let plugins = [
+	del({
+		targets: [pkg.output]
+	}),
+	resolve({
+		extensions: [".js", ".ts"]
+	}),
+	importCss(),
+	sucrase({
+		production: true,
+		exclude: ["node_modules/**"],
+		jsxPragma: "h",
+		transforms: ["typescript", "jsx"]
+	}),
+	workbox({
+		globDirectory: publicDir,
+		globPatterns: ["index.html", "**/*.{js,css}"],
+		swDest: publicDir + "/sw.js",
+		navigateFallback: "index.html"
+	})
+];
+
+if (process.env.ROLLUP_WATCH) {
+	plugins.push(browsersync({ single: true, server: publicDir }));
+} else {
+	process.env.BUILD = "production";
+	plugins.push(terser());
+}
+
+export default {
+	input: pkg.source,
+	output: [
+		{
+			dir: pkg.output,
+			format: "esm",
+			sourcemap: true
+		}
+	],
+	plugins
+};
---
+++ atomico-todo/src/index.js
@@ -0,0 +1,52 @@
+import { useState } from "@atomico/core";
+import { h, customElement } from "@atomico/element";
+
+import TodoInput from "./web-components/todo-input";
+import TodoItem from "./web-components/todo-item";
+
+import style from "./style.css";
+
+function Todo({ task = [] }) {
+	let [state, setState] = useState(task);
+	return (
+		<host shadowDom>
+			<style>{style}</style>
+			<TodoInput
+				placeholder="What needs to be done?"
+				handlerChange={task => {
+					setState(state.concat(task));
+				}}
+			/>
+			<div>
+				{state.map(({ text, checked, id }, localIndex) => (
+					<TodoItem
+						key={id}
+						text={text}
+						checked={checked}
+						handlerRemove={() => {
+							setState(state.filter((data, index) => index !== localIndex));
+						}}
+						handlerToggle={() => {
+							setState(
+								state.map((data, index) =>
+									index === localIndex
+										? {
+												...data,
+												checked: !data.checked
+										  }
+										: data
+								)
+							);
+						}}
+					/>
+				))}
+			</div>
+		</host>
+	);
+}
+
+Todo.observables = {
+	task: Array
+};
+
+customElement("atomico-todo", Todo);
